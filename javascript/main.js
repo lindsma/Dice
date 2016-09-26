4b52d13b90531e1d4720dd0cfc78cfe4376054b8
Lindsey Adams <linzma87@gmail.com>
2016-09-26 15:08:19 -0400

    HTML/CSS structure

---
+++ javascript/main.js
@@ -0,0 +1,22 @@
+var gameObject = {
+  // start date/time of game
+  startTime: null,
+  // array of game rounds - Each round lasts until the user wins, so one round should have the start time of that round plus the number of rolls of the dice that have occurred for that round.
+  gameRounds: [],
+
+  // The game object should have a function (method) for rolling the dice
+
+  rollDice: function () {
+    var roll = Math.floor(Math.random())
+
+
+  }
+
+  // display start date/time at bottom of page when page is loaded
+
+
+
+
+
+
+}
